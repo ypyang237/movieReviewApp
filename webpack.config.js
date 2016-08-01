8b7a842db3afa96ddd325c94e9b2c376d3e86517
ypyang237 <ypyang237@gmail.com>
2016-08-01 10:50:24 +1000

    react hello world set up

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './react-src/index.js',
+  output: {
+    filename: './public/js/browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+    ]
+  }
+};
