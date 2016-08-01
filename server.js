cb3b99a3893b25dfbf61a11869ad4ad1aac4acbf
ypyang237 <ypyang237@gmail.com>
2016-08-01 10:54:00 +1000

    adds body parser json

--- server.js
+++ server.js
@@ -9,11 +9,21 @@ const express    = require('express'),
 const app   = express(),
       PORT  = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json())
+  ;
 
 app.use('/api', api);
 app.use(express.static('public'));
 
+app.use(express.static(__dirname + "/public"));
+
+app.get('*', function(req, res){
+  res.sendFile('./public/index.html',
+    {
+      root  : __dirname
+    });
+});
 
 app.listen(PORT, function() {
   console.log(`Server listening on port: ${PORT}`);
